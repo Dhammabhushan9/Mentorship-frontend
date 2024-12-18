70a02e8f4beb5acc90eb3377c6cec79569178312
Dhammabhushan9 <dhammwagh922004@gmail.com>
2024-12-18 21:28:17 +0530

    setup for vercel

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`https://mentorship-backend-chi.vercel.app/api/auth/login`, {
+      const response = await axios.post(`api/auth/login`, {
         email,
         password,
       });
